1c4f75981f0880caf0648811d08e03700ec56a40
ishaban <ishaban.flash@gmail.com>
2022-07-14 21:18:43 +0400

    add bootstrap css and roboto fonts

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,6 +1,6 @@
 module DashboardHelper
   def mock_data
-    {"freeValue": 50, "assessedValue": 100, "investedValue": 200, "income": "300"}
+    {"freeValue": '51 234 $', "assessedValue": '100 432 $', "investedValue": '200 $', "income": "300 $"}
   end
 
   def render_header
