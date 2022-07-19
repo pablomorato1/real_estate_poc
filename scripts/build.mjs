5a93a8999ae0aa64ff6bf3ca2937b10ca60632bd
ishaban <ishaban.flash@gmail.com>
2022-07-19 12:57:12 +0400

    run stimulus as part of dev build

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,10 +1,14 @@
 import { globPlugin } from 'esbuild-plugin-glob';
 import esbuild from "esbuild";
 
+const cliArgs = process.argv.slice(2);
+const watch = cliArgs.includes('--watch');
+
 esbuild.build({
   entryPoints: ['./app/javascript/*.*'],
   bundle: true,
   sourcemap: true,
+  watch,
   outdir: './app/assets/builds',
   plugins: [globPlugin()],
 });
