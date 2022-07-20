ea5729666d185d8a5571070c7c96ab8e07b00517
pablomorato1 <pablomorato_09@hotmail.com>
2022-07-20 14:04:50 -0300

    feat: Add property seed and update the attributes on promotions list page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+if Property.all.empty?
+  10.times do |index|
+    index = index + 1
+    image = URI.parse(Faker::LoremFlickr.image(search_terms: ['building'])).open
+
+    property = Property.create(
+      name: "Property #{index}",
+      address: "Sample's street, #{100 + index}",
+      area: rand(100...250).to_f,
+      yield_plan: "#{rand(5...10)}-#{rand(11..15)}%",
+      price: rand(100000...250000).to_f,
+      managed_equity: 100.0,
+      min_investment: rand(1000...5000).to_f,
+      prop_type: "passive",
+      min_equity: rand(1..15).to_f,
+      presentation_url: Faker::LoremFlickr.image
+    )
+
+    property.image.attach(io: image, filename: "property_#{index}_image.jpg")
+  end
+end
