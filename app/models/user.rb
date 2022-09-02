6c4d3a297ca70a31483f0bf9f8ecf5e8942c976f
pablomorato1 <pablomorato_09@hotmail.com>
2022-09-02 17:04:11 -0300

    feat: Create user mailer to deliver their credentials after its creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  after_create :send_credentials_email
   after_create :create_wallet
 
 
   enum role: %i[guest admin client]
 
-  has_many :investments, inverse_of: :user
   has_one :wallet
+  has_many :investments, inverse_of: :user
   has_many :transactions, through: :wallet
 
-  validates :name, :email, :password, presence: :true
+  validates :name, :email, presence: :true
 
   private
+
+  def send_credentials_email
+    UserMailer.with(user: self, password: self.password)
+    .first_access.deliver_later
+  end
 
   def create_wallet
     wallet = Wallet.create
